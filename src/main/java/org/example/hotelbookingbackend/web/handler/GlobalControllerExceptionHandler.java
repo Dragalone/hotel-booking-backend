11cf84ae39482b0964ed909bbf907b73ebad4855
Vladimir <vova.kaverya@mail.ru>
2024-08-06 21:55:19 +0300

    exceptions handler

---
+++ src/main/java/org/example/hotelbookingbackend/web/handler/GlobalControllerExceptionHandler.java
@@ -0,0 +1,32 @@
+package org.example.hotelbookingbackend.web.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.hotelbookingbackend.exception.*;
+import org.example.hotelbookingbackend.web.dto.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalControllerExceptionHandler {
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> alreadyExists(AlreadyExistsException ex) {
+        log.error("Authentication is required to access this resource.", ex);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> notFound(EntityNotFoundException ex) {
+        log.error("Error trying to get entity", ex);
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(ex.getMessage()));
+    }
+
+}
